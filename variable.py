79589af646cdd49fd0249b3fffd6eb531dec9705
Michael Cutsail <65092568+vader2010@users.noreply.github.com>
2023-06-23 09:06:16 -0700

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,28 @@
+'''
+Rules for Variables:
+1. the variable name can only include digits, _, a to z, and A to Z
+2. the variable name cannot start with a digit
+3. the variable name cannot have a space in it
+
+Ways to print variables:
+print("blah blah blah ", blah)
+print(f"blah blah blah {blah}") (best way)
+print("blah blah blah {}".format(blah))
+'''
+'''
+Arithematic:
++ is add
+- is subtract
+* is multiply
+/ is divide
+% is subtract until number less than
+// is rounded division
+
+Examples:
+addition = number1 + number 2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+modulo = number1 % number2
+rounded division = number1 // number2
+'''
