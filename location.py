285fb8392ab3f1c99f015e86e32f0cf12dcd7323
Michael Cutsail <65092568+vader2010@users.noreply.github.com>
2023-06-23 10:15:32 -0700

    Create location.py

---
+++ location.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+address = input("Enter your address: ")
+city = input("Enter your city: ")
+state = input("Enter your state: ")
+zip_ = input("Enter your zip code: ")
+
+print(f"\nHello, {name}\nYou are located at {address}, {city} in {state}\nYour zipcode is {zip_}")
