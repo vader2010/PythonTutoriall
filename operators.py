a7d455edb2384677710118f28f5f70aa4b7319dd
Michael Cutsail <65092568+vader2010@users.noreply.github.com>
2023-06-23 09:48:09 -0700

    Update operators.py

--- operators.py
+++ operators.py
@@ -1,5 +1,12 @@
+from math import pi
+
 number1 = eval(input("Enter number1: "))
 number2 = eval(input("Enter number2: "))
+diameter = eval(input("Enter diameter: "))
+side1 = eval(input("Enter side1: "))
+side2 = eval(input("Enter side2: "))
+radius = diameter/2
+radius_squared = radius*radius
 
 addition = number1 + number2
 subtraction = number1 - number2
@@ -14,3 +21,5 @@ print(f"{number1} * {number2} = {multiplication}")
 print(f"{number1} / {number2} = {division}")
 print(f"{number1} % {number2} = {modulo}")
 print(f"{number1} // {number2} = {rounded_division}")
+print(f"Area of Circle: {round((2*pi*radius_squared)/2)}")
+print(f"Area of Rectangle: {side1*side2}")
