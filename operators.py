f099145ef49acdecdcbafdde70daedbfc2191009
Michael Cutsail <65092568+vader2010@users.noreply.github.com>
2023-06-23 09:29:53 -0700

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,16 @@
+number1 = eval(input("Enter number1: "))
+number2 = eval(input("Enter number2: "))
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+modulo = number1 % number2
+rounded_division = number1 // number2
+
+print(f"{number1} + {number2} = {addition}")
+print(f"{number1} - {number2} = {subtraction}")
+print(f"{number1} * {number2} = {multiplication}")
+print(f"{number1} / {number2} = {division}")
+print(f"{number1} % {number2} = {modulo}")
+print(f"{number1} // {number2} = {rounded_division}")
